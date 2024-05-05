45242eaed6a0701e0b0d28837bc14b8445cd0cd7
Selim Dag <sdagtasoglu@gmail.com>
2024-05-05 15:29:40 +0200

    Facelift v1

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 window.onload = function() {
-    var contentDiv = document.getElementById('content');
+    var messageElement = document.getElementById('message');
+    var imageElement = document.getElementById('image');
+    var daysLeftElement = document.getElementById('daysLeft');
+
     var today = new Date();
     var birthday = new Date(today.getFullYear(), 2, 4); // Month is 0-indexed, so 2 is March
 
     if (today.getMonth() == birthday.getMonth() && today.getDate() == birthday.getDate()) {
-        contentDiv.innerHTML = "<h1>Yes, it's the 4th of March! 🎉🎂🎈</h1><img src='happy_picture.jpg' alt='Happy Picture'>";
+        messageElement.textContent = "Yes, it's the 4th of March! 🎉🎂🎈";
+        imageElement.src = 'happy_picture.jpg';
     } else {
         if (today > birthday) {
             birthday.setFullYear(birthday.getFullYear() + 1);
         }
         var oneDay = 1000 * 60 * 60 * 24;
         var daysLeft = Math.ceil((birthday.getTime() - today.getTime()) / oneDay);
-        contentDiv.innerHTML = "<h1>No, it's not the 4th of March 😞</h1><p>Only " + daysLeft + " days left!</p>";
+        messageElement.textContent = "No, it's not the 4th of March 😞";
+        daysLeftElement.textContent = "Only " + daysLeft + " days left!";
     }
 }
