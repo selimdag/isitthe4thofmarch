c830315f8e3a60e7e9f6870c97e624d12e4baad1
Selim Dag <sdagtasoglu@gmail.com>
2024-08-03 09:25:15 +0200

    Change 3rd August to 4th

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ window.onload = function() {
             imageElement.src = 'happy_picture.jpeg';
             daysLeftElement.textContent = "Happy birthday Johannaaaaaaaa!";
             createEmojiRain(['🎊', '🎉', '🍸', '🍹', '🍾', '👯‍♀️', '🎂', '🥳', '🎆']);
-        } else if (now.getMonth() == 7 && now.getDate() == 3) {
-            messageElement.textContent = "No, it's not the 4th of March yet. But it's 3rd August and we are celebrating. Johanna is 11111 days old tomorrow!!! 🥳";
+        } else if (now.getMonth() == 7 && now.getDate() == 4) {
+            messageElement.textContent = "No, it's not the 4th of March yet. But it's 4th August. Johanna is 11111 days old today!!! 🥳";
             imageElement.src = 'happy_picture.jpeg';
             daysLeftElement.textContent = "And only " + days + " days, " + hours + " hours, " + mins + " minutes, and " + secs + " seconds left till 4th March!";
             createEmojiRain(['🎊', '🎉', '🍸', '🍹', '🍾', '👯‍♀️', '🎂', '🥳', '🎆']);
