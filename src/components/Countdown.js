f7ebb5824f76cecb0935059e71a7178374376da4
Selim Dag <sdagtasoglu@gmail.com>
2025-03-02 12:18:11 +0100

    Change birthday message and add bouncing

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -5,11 +5,19 @@ function Countdown({ isBirthday, timeLeft }) {
   const { days, hours, minutes, seconds } = timeLeft;
   
   return (
-    <h2 className="countdown">
-      {isBirthday
-        ? "Happy birthday Johannaaaaaaaa!"
-        : `But only ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds left!`}
-    </h2>
+    <div className="countdown-container">
+      <h2 className="countdown">
+        {isBirthday
+          ? "Happy birthday Johannaaaaaa! 🎉"
+          : `But only ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds left!`}
+      </h2>
+      
+      {isBirthday && (
+        <h3 className="additional-birthday-message">
+          And all the other nice people around the world, who have birthdays today! 🥳
+        </h3>
+      )}
+    </div>
   );
 }
 
